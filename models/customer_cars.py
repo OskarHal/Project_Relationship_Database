e6790b46409989ea9e1bfaae6e6c88289924a392
Andreas Gustafsson <andreas.gustafsson88@gmail.com>
2020-11-13 15:54:17 +0100

    Added content again.. to the new classes

--- models/customer_cars.py
+++ models/customer_cars.py
@@ -1,6 +1,22 @@
-def main():
-    pass
+import sqlalchemy as sa
+from sqlalchemy.orm import relationship
+from app.db import Base
 
 
-if __name__ == "__main__":
-    main()
+class CustomerCar(Base):
+    __tablename__ = 'customer_cars'
+    registration_nr = sa.Column(sa.String(45), primary_key=True, autoincrement=False)
+    customer_id = sa.Column(sa.Integer, sa.ForeignKey("customers.customer_id"))
+    brand = sa.Column(sa.String(45), nullable=False)
+    model = sa.Column(sa.String(45), nullable=False)
+    model_year = sa.Column(sa.Integer, nullable=False)
+    color = sa.Column(sa.String(45), nullable=False)
+    owner = relationship("Customer", back_populates="cars")
+
+    def __repr__(self):
+        return f'Registration number={self.registration_nr}, ' \
+               f'Car brand={self.brand}, ' \
+               f'Car Model={self.model} ', \
+               f'Model year={self.model_year}', \
+               f'Color={self.color}',
+
