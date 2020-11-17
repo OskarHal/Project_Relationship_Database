e8733671d884cdb12c4bac4e1d1a81377ee5ec69
Oskar H <oskar.hallin@gmail.com>
2020-11-17 16:08:18 +0100

    adds tables to migrations

--- migrations/versions/4add503fdd14_create_table_orders.py
+++ migrations/versions/4add503fdd14_create_table_orders.py
@@ -7,7 +7,7 @@ Create Date: 2020-11-17 15:07:09.710487
 """
 from alembic import op
 import sqlalchemy as sa
-
+from datetime import datetime
 
 # revision identifiers, used by Alembic.
 revision = '4add503fdd14'
@@ -17,8 +17,15 @@ depends_on = None
 
 
 def upgrade():
-    pass
+    op.create_table(
+        'orders',
+        sa.Column('order_id', sa.Integer, primary_key=True),
+        sa.Column('customer_id', sa.Integer, sa.ForeignKey('customers.customer_id'), nullable=True),
+        sa.Column('employee_id', sa.Integer, sa.ForeignKey('employees.employee_id'), nullable=True),
+        sa.Column('store_id', sa.Integer, sa.ForeignKey('stores.store_id'), nullable=True),
+        sa.Column('order_date', sa.DateTime, default=datetime.utcnow, nullable=False)
+    )
 
 
 def downgrade():
-    pass
+    op.drop_table('orders')
