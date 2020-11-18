48f3d8d15e9d8e5f0fe3105a813bdc9ff6319e7c
oscarwiding <65022048+oscarwiding@users.noreply.github.com>
2020-11-18 15:55:45 +0100

    Update b45264bc0b96_create_table_spareparts.py

--- app/Data/migrations/versions/b45264bc0b96_create_table_spareparts.py
+++ app/Data/migrations/versions/b45264bc0b96_create_table_spareparts.py
@@ -17,8 +17,20 @@ depends_on = None
 
 
 def upgrade():
-    pass
+    op.create_table(
+        'spare_parts',
+        sa.Column('spare_part_id', sa.Integer, primary_key=True),
+        sa.Column('product_nr', sa.String(45), nullable=False),
+        sa.Column('description', sa.String(255), nullable=True),
+        sa.Column('purchase_price', sa.FLOAT, nullable=False),
+        sa.Column('selling_price', sa.FLOAT, nullable=False),
+        sa.Column('reorder_level', sa.Integer, nullable=True),
+        sa.Column('order_quantity', sa.Integer, nullable=True),
+        sa.Column('estimated_time_of_arrival', sa.DATE, nullable=True),
+        sa.Column('manufacturer_id', sa.Integer, sa.ForeignKey('manufacturers.manufacturer_id')),
+        sa.Column('supplier_id', sa.Integer, sa.ForeignKey('suppliers.supplier_id')),
+    )
 
 
 def downgrade():
-    pass
+    op.drop_table('spare_parts')
