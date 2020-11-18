ac7538fab6ccaaa737d6fef0b2feb5d4062111b7
Astrid Dahlen <astriddahlen@hotmail.com>
2020-11-18 16:58:28 +0100

    Updated Models

--- models/car_models_spare_parts.py
+++ models/car_models_spare_parts.py
@@ -4,6 +4,6 @@ from db import Base
 car_models_spare_parts = sa.Table(
     'car_models_spare_parts',
     Base.metadata,
-    sa.Column('car_model_id', sa.Integer, sa.ForeignKey('car_models.car_model_id')),
-    sa.Column('spare_part_id', sa.Integer, sa.ForeignKey('spare_parts.spare_part_id'))
+    sa.Column('car_model_id', sa.Integer, sa.ForeignKey('car_models.car_model_id'), ondelete= 'RESTRICT', onupdate='CASCADE'),
+    sa.Column('spare_part_id', sa.Integer, sa.ForeignKey('spare_parts.spare_part_id'), ondelete= 'RESTRICT', onupdate='NO ACTION')
 )
--- models/spare_part_stores.py
+++ models/spare_part_stores.py
@@ -5,8 +5,8 @@ from db import Base
 
 class SparePartStore(Base):
     __tablename__ = 'spare_part_stores'
-    store_id = sa.Column(sa.Integer, sa.ForeignKey('stores.store_id'), primary_key=True)
-    spare_part_id = sa.Column(sa.Integer, sa.ForeignKey('spare_parts.spare_part_id'), primary_key=True)
+    store_id = sa.Column(sa.Integer, sa.ForeignKey('stores.store_id'), primary_key=True,ondelete= 'CASCADEL', onupdate='CASCADE'))
+    spare_part_id = sa.Column(sa.Integer, sa.ForeignKey('spare_parts.spare_part_id'), primary_key=True,ondelete= 'CASCADEL', onupdate='CASCADE'))
     stock = sa.Column(sa.Integer, nullable=False)
     stock_location = sa.Column(sa.String(45), nullable=False)
     spare_part = relationship('SparePart', back_populates="stores")
