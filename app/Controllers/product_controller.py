67864a7689659c4943456c96a3cba847279d65fc
Oskar H <oskar.hallin@gmail.com>
2020-11-23 13:28:27 +0100

    refactors, adds delete with solved delete problem and adds new menu system for products

--- app/Controllers/product_controller.py
+++ app/Controllers/product_controller.py
@@ -1,14 +1,19 @@
+from Data.Models.spare_parts import SparePart
 from Data.Repository import products_repository as pr
 
 
-def get_all_products():
+def get_all_products() -> None:
     return pr.get_all_products()
 
 
-def get_product_by_product_nr(product_nr: str):
+def get_product_by_product_nr(product_nr: str) -> SparePart:
     return pr.get_product_by_product_nr(product_nr)
 
 
-def product_by_product_description(description_pattern):
-    products = pr.get_product_by_product_description(description_pattern)
+def get_products_by_product_description_pattern(description_pattern: str) -> {}:
+    products = pr.get_products_by_product_description_pattern(description_pattern)
     return {i + 1: products for i, products in enumerate(products)}
+
+
+def delete_product(product) -> bool:
+    return pr.delete_product(product)
