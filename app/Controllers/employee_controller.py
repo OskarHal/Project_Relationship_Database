7a75eca3e0cb5779a9cf32136165db41a884dadc
Andreas Gustafsson <andreas.gustafsson88@gmail.com>
2020-11-24 14:38:10 +0100

    updated fire employee

--- app/Controllers/employee_controller.py
+++ app/Controllers/employee_controller.py
@@ -1,4 +1,5 @@
 import Data.Repository.employees_repository as er
+import Data.Repository.customer_cars_repository as ccr
 
 
 def get_all_employees():
@@ -8,3 +9,16 @@ def get_all_employees():
 def get_employee_by_id(selection):
     return er.get_employee_by_id(selection)
 
+
+def fire_employee(employee):
+    return er.fire_employee(employee)
+
+
+def theft(thief):
+    amount, item = er.steal_products(thief)
+    car = ccr.steal_car()
+    if car.owner.customer_type == 1:
+        owner = car.owner.priv_customer[0].private_customer_first_name
+    elif car.owner.customer_type == 2:
+        owner = car.owner.comp_customer[0].company_customer_company_name
+    return car, amount, item, owner
--- app/Data/Repository/employees_repository.py
+++ app/Data/Repository/employees_repository.py
@@ -1,5 +1,7 @@
 from Data.Models.employees import Employee
+from Data.Models.spare_part_stores import SparePartStore
 from Data.db import session
+import random
 
 
 def get_all_employees():
@@ -9,3 +11,19 @@ def get_all_employees():
 def get_employee_by_id(selection):
     return session.query(Employee).filter_by(employee_id=selection).first()
 
+
+def fire_employee(employee):
+    try:
+        session.query(Employee).filter_by(employee_id=employee.employee_id).delete()
+        session.commit()
+    except:
+        session.rollback()
+
+
+def steal_products(thief):
+    item = session.query(SparePartStore).filter(SparePartStore.store_id == thief.store_id) \
+        .filter(SparePartStore.spare_part_id == random.choice([1, 2, 3, 4])).first()
+    amount = item.stock
+    item.stock = 0
+    session.commit()
+    return amount, item.spare_part.description
