a2bcf3b7885251fb00892d90f459ea7374a47bb6
Oscar Widing <widing.oscar@gmail.com>
2020-11-24 11:29:45 +0100

    show stock in diffrent store, with enough wares in stock

--- app/Data/Repository/order_repository.py
+++ app/Data/Repository/order_repository.py
@@ -32,6 +32,7 @@ def update_stock(old_stock, quantity):
         session.rollback()
 
 
-def find_diffrent_store(spare_part_id, store_id):
+def find_diffrent_store(spare_part_id, store_id, quantity):
     return session.query(SparePartStore).filter(SparePartStore.spare_part_id.like(f'{spare_part_id}')). \
-        filter(SparePartStore.store_id.notlike(f'{store_id}')).all()
+        filter(SparePartStore.store_id.notlike(f'{store_id}')).\
+        filter(SparePartStore.stock >= quantity).all()
